0f69f102d70c9f6eb39f7c4cfded9cf6218c6e64
Veneta Stefanova <venetastefanovaa@gmail.com>
2018-12-12 16:43:50 +0200

    removing links

---
+++ postgres/deploy_schemas.sql
@@ -0,0 +1,8 @@
+-- Deploy fresh database tables
+
+--\i  executes scripts
+
+\i '/docker-entrypoint-initdb.d/tables/users.sql'
+\i '/docker-entrypoint-initdb.d/tables/login.sql'
+
+\i '/docker-entrypoint-initdb.d/seed/seed.sql'
